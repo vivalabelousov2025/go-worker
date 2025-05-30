9fd60465b1c2f9125e39d35ef60871982ee2f327
Sussy <mranimatory@gmail.com>
2025-05-30 16:28:03 +0300

    add service

--- internal/rest/handlers.go
+++ internal/rest/handlers.go
@@ -4,17 +4,32 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/vivalabelousov2025/go-worker/internal/dto"
+	"github.com/vivalabelousov2025/go-worker/pkg/logger"
+	"go.uber.org/zap"
 )
 
-type Handlers struct {
+type Service interface {
 }
 
-func NewHandlers() *Handlers {
-	return &Handlers{}
+type Handlers struct {
+	service Service
+}
+
+func NewHandlers(service Service) *Handlers {
+	return &Handlers{service: service}
 }
 
 func (h *Handlers) OrderProcess(c echo.Context) error {
 
-	return c.JSON(http.StatusOK, "test")
+	ctx := c.Request().Context()
+	var reqSturct dto.Order
+
+	if err := c.Bind(&reqSturct); err != nil {
+		logger.GetLoggerFromCtx(ctx).Info(ctx, "error", zap.Error(err))
+		return err
+	}
+
+	return c.JSON(http.StatusOK, reqSturct)
 
 }
